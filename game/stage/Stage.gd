d277a08020f2fc11398364bab1b459be6e34d65b
James Zhong <thebitspud@gmail.com>
2023-04-13 23:39:30 -0700

    Implemented basic loss condition

--- game/stage/Stage.gd
+++ game/stage/Stage.gd
@@ -11,10 +11,12 @@ var turn_number := 0
 var max_turns := 10 # TODO: replace
 var time_elapsed := 0.0
 var time_limit := 15 # TODO: replace
+var setup_phase := true
+
 
 ## Called when the node enters the scene tree for the first time.
 func _ready():
-	_next_turn()
+	_end_turn()
 
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,11 +25,15 @@ func _process(delta: float):
 	_update_hover()
 	
 	if Input.is_action_just_pressed("ui_accept"):
-		_next_turn()
+		_start_turn() if setup_phase else _end_turn()
 
 
 func _update_time(delta: float):
-	time_elapsed += delta
+	if not setup_phase:
+		time_elapsed += delta
+		if time_elapsed > time_limit:
+			_end_turn()
+	
 	_time_label.text = "Seconds Left: " + str(ceil(time_limit - time_elapsed))
 
 
@@ -44,8 +50,17 @@ func _update_hover():
 		_tile_info.text = ""
 
 
-## Advances the current game by one turn
-func _next_turn():
+func _start_turn():
+	setup_phase = false
+
+
+## Completes the current turn and advances the current game by one turn
+func _end_turn():
 	turn_number += 1
+	
+	if turn_number > max_turns:
+		get_tree().change_scene("res://screens/loss/LossScreen.tscn")
+	
+	setup_phase = true
 	time_elapsed = 0.0
 	_turn_label.text = "Turn " + str(turn_number) + "/" + str(max_turns)
---
+++ screens/loss/LossScreen.gd
@@ -0,0 +1,20 @@
+extends Panel
+
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		_retry_level()
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		_quit_to_title()
+
+
+## Renitializes and replays the last attempted level
+func _retry_level():
+	get_tree().change_scene("res://screens/play/PlayScreen.tscn")
+
+
+## Sends the user back to the title screen
+func _quit_to_title():
+	get_tree().change_scene("res://screens/title/TitleScreen.tscn")
