419f7bdf09135df6ef772894fd4d2e039643a483
James Zhong <thebitspud@gmail.com>
2023-04-14 10:28:45 -0700

    Added custom game camera

---
+++ game/stage/Camera.gd
@@ -0,0 +1,51 @@
+extends Camera2D
+
+
+export var scroll_speed: float = 250
+
+
+onready var _map : TileMap = $"../Map"
+var _stage_scale := 3.0;
+
+
+## Called when the node enters the scene tree for the first time.
+func _ready():
+	_reset_position()
+	adjust_stage_scale(0)
+
+
+## Called at a constant rate. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float):
+	var scroll_dist := (scroll_speed * delta) / sqrt(_stage_scale)
+	
+	if Input.is_action_pressed("ui_left"):
+		position.x -= scroll_dist
+	
+	if Input.is_action_pressed("ui_right"):
+		position.x += scroll_dist
+	
+	if Input.is_action_pressed("ui_up"):
+		position.y -= scroll_dist
+	
+	if Input.is_action_pressed("ui_down"):
+		position.y += scroll_dist
+	
+	if Input.is_action_pressed("ui_zoom_in"):
+		adjust_stage_scale(delta)
+	
+	if Input.is_action_pressed("ui_zoom_out"):
+		adjust_stage_scale(-delta)
+
+
+## Move the camera's view to the middle of the map
+func _reset_position():
+	var map_size = _map.cell_size * _map.get_used_rect().size
+	print(map_size)
+	position = map_size / 2;
+
+
+## Increases or decreases the camera's zoom factor by the specified amount
+func adjust_stage_scale(value: float):
+	_stage_scale = clamp(_stage_scale + value, 1, 3)
+	var zoom_factor := 1.0/_stage_scale;
+	zoom = Vector2(zoom_factor, zoom_factor)
--- game/stage/Map.gd
+++ game/stage/Map.gd
@@ -1,9 +1,15 @@
 extends TileMap
 
 
+onready var _camera : Camera2D = $"../Camera"
+
+
 ## Returns the cell the user's mouse is currently hovering over
 func get_mouse_cell() -> Vector2:
-	return world_to_map(get_viewport().get_mouse_position() / 3)
+	var camera_position = _camera.get_camera_position()
+	var mouse_position = get_viewport().get_mouse_position()
+	var mouse_offset = (mouse_position - get_viewport().size / 2) * _camera.zoom
+	return world_to_map(camera_position + mouse_offset)
 
 
 ## Returns true if a non-empty tile exists at the location of the specified cell
