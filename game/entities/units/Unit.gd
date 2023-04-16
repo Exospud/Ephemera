bdd01441f19977688915f0acd09acb30c56bff52
James Zhong <thebitspud@gmail.com>
2023-04-16 00:04:53 -0700

    Added simple unit, tower, projectile sprites

--- game/entities/units/Unit.gd
+++ game/entities/units/Unit.gd
@@ -6,10 +6,12 @@ export(Vector2) var velocity := Vector2.RIGHT # tiles/second
 export var attack_damage := 1
 export var attack_rate := 1
 
+onready var sprite := $Sprite
+
 
 ## Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	sprite.position += Vector2(randi() % 5 - 2, randi() % 9 - 4)
 
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
